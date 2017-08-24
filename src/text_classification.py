cfec845c0b04edb90488c32d28ad4a95dda56706
Duc N. Trinh <duc.n.trinh@gmail.com>
2017-08-25 01:09:15 +0700

    Add config argument for text classification

--- src/text_classification.py
+++ src/text_classification.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Import
+from argparse import ArgumentParser
 import numpy as np
 from gensim.models import KeyedVectors
 from keras.preprocessing import sequence
@@ -9,19 +9,39 @@ from pymongo import MongoClient
 
 from deep_text_cnn import DeepTextCNN
 
-# Load database
-mongo_client = MongoClient()
-database = mongo_client.get_database('test')
-train_collection = database.get_collection('train')
-test_collection = database.get_collection('test')
-vocab_collection = database.get_collection('vocab')
+ARGS_PARSER = ArgumentParser()
+ARGS_PARSER.add_argument(
+    '-d', '--database', type=str, help='Name of database', required=True)
+ARGS_PARSER.add_argument(
+    '-e',
+    '--embedding',
+    type=str,
+    help='Path to Google News word2vec pretrained file',
+    required=True)
+ARGS_PARSER.add_argument(
+    '-n', '--numclasses', type=int, help='Number of classes', required=True)
+ARGS = ARGS_PARSER.parse_args()
 
-EMBEDDING_FILE = '/home/ductn/Downloads/GoogleNews-vectors-negative300.bin'
+DATABASE_NAME = ARGS.database
+EMBEDDING_FILE = ARGS.embedding
+NUM_CLASSES = ARGS.numclasses
+
 SEQUENCE_LENGTH = 200
-NUM_CLASSES = 5
+VOCAB_SIZE = 30000
+EMBEDDING_SIZE = 300
+FILTER_SIZES = [3, 4, 5]
+NUM_FILTERS = 150
+DROPOUT_KEEP_PROB = float(0.5)
 
-TRAIN_SIZE = train_collection.count()
-TEST_SIZE = test_collection.count()
+# Load database
+MONGO_CLIENT = MongoClient()
+DATABASE = MONGO_CLIENT.get_database(DATABASE_NAME)
+TRAIN_COLLECTION = DATABASE.get_collection('train')
+TEST_COLLECTION = DATABASE.get_collection('test')
+VOCAB_COLLECTION = DATABASE.get_collection('vocab')
+
+TRAIN_SIZE = TRAIN_COLLECTION.count()
+TEST_SIZE = TEST_COLLECTION.count()
 
 
 def rating_to_one_hot(rating):
@@ -51,19 +71,11 @@ def data_generator(collection, batch_size):
             k += 1
 
 
-# Create model
-VOCAB_SIZE = 30000
-EMBEDDING_SIZE = 300
-FILTER_SIZES = [3, 4, 5]
-NUM_FILTERS = 150
-DROPOUT_KEEP_PROB = float(0.5)
-
-
 def prepare_embedding():
     word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
 
     embedding_weights = np.zeros((VOCAB_SIZE, EMBEDDING_SIZE))
-    for document in vocab_collection.find({}):
+    for document in VOCAB_COLLECTION.find({}):
         word = document['word']
         index = document['index']
         if index > VOCAB_SIZE:
@@ -75,7 +87,7 @@ def prepare_embedding():
 
 
 EMBEDDING_WEIGHTS = prepare_embedding()
-model = DeepTextCNN(SEQUENCE_LENGTH, NUM_CLASSES, VOCAB_SIZE, EMBEDDING_SIZE,
+MODEL = DeepTextCNN(SEQUENCE_LENGTH, NUM_CLASSES, VOCAB_SIZE, EMBEDDING_SIZE,
                     EMBEDDING_WEIGHTS, FILTER_SIZES, NUM_FILTERS,
                     DROPOUT_KEEP_PROB)
 
@@ -85,12 +97,12 @@ EPOCHS = 10
 STEPS_PER_EPOCH = TRAIN_SIZE / BATCH_SIZE
 VALIDATION_STEPS = TEST_SIZE / BATCH_SIZE
 
-CSV_LOGGER = CSVLogger('log.csv', append=True, separator=';')
-model.summary()
-model.fit_generator(
-    data_generator(train_collection, BATCH_SIZE),
+CSV_LOGGER = CSVLogger(DATABASE_NAME + '_log.csv', append=True, separator=',')
+MODEL.summary()
+MODEL.fit_generator(
+    data_generator(TRAIN_COLLECTION, BATCH_SIZE),
     steps_per_epoch=STEPS_PER_EPOCH,
     epochs=EPOCHS,
-    validation_data=data_generator(test_collection, BATCH_SIZE),
+    validation_data=data_generator(TEST_COLLECTION, BATCH_SIZE),
     validation_steps=VALIDATION_STEPS,
     callbacks=[CSV_LOGGER])
